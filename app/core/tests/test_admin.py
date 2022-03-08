f4c852b73f7c8e24acf30dcbac03968866a0a348
saphal shrestha <success123shrestha@gmail.com>
2022-03-08 23:06:34 +0545

    updated admin to create custom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,49 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        """"""
+
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='ab@gmail.com',
+            password='password'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='password',
+            name='test user full name'
+        )
+
+
+    def test_users_listed(self):
+        """Test that user are listed on user page"""
+
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    
+    def test_user_change_page(self):
+        """Tets that user edit page works"""
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEquals(res.status_code, 200)
+
+
+    def test_create_user_page(self):
+        """Test if user page works"""
+
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
